df11389c2caf01b42f34a282376ee456d412d663
Caroline Trombeta <carolinecampost@gmail.com>
2022-10-13 16:30:24 +0100

    13/10 - my first commit in this project! :)

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import gato from './assets/gato.jpg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App container">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={gato} className="gato" alt="React logo" />
+        </a>
+      </div>
+      <h1>FUCK OFF</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 2)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
