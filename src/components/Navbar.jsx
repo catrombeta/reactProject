947bed9a08838002efb1c329f1083919854d9447
Caroline Trombeta <carolinecampost@gmail.com>
2022-11-01 12:28:37 +0000

    01/11 switched assets folder to public folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
         <nav>
             <div className="row justify-content-around">
                 <div className="col-md-2">
-                    <img src="../../src/assets/logo.png" className="logo-navbar" />
+                    <img src="../../public/assets/logo.png" className="logo-navbar" />
                 </div>
                 <div className="col-md-2">
                     <h3>
